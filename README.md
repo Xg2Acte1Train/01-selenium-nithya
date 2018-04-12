6df9cd48b4399c981d6cdcf788ac5865ff5bcc58
Nithya Duraisamy <nithyadurai87@gmail.com>
2018-04-12 20:44:47 +0530

    Added documents

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 Script to automate amazon with selenium and python
+
+
+# Task
+
+
+1. Open Amazon.in in firefox
+2. search for "data catalog"
+3. Get result page
+4. Check for first result
+5. Get the book title
+6. Get the author name
+7. Get Paperback price
+8. Get Hardcover Price
+
+
+## Missing : 
+Missed to select "Books" on the dropdown list in the home page.
+
+
+
+### Language of Choice: 
+Python
+
+#### Reasons : 
+It is powerful high level language. It provides required modules for all the automation requirements. Anyone can learn it faster.
+
+
+### Test Framework : 
+Unittest for Python
+
+##### Reasons : 
+unittest supports test automation, sharing of setup and shutdown code for tests, aggregation of tests into collections, and independence of the tests from the reporting framework. The unittest module provides classes that make it easy to support these qualities for a set of tests.
+
+
+### Browser : 
+Mozilla Firefox
+
+#### Reasons :
+Free/Open Source Software. 
+Protects Privacy
+Provides great speed
+
+
+
--- test_amazon.py
+++ test_amazon.py
@@ -7,18 +7,17 @@ from selenium.common.exceptions import NoSuchElementException
 class Amazon(unittest.TestCase):
     @classmethod
     def setUp(cls):
+        # Call Firefox browser
         cls.driver = webdriver.Firefox(executable_path=r'./geckodriver')
         cls.driver.implicitly_wait(30)
-       # cls.base_url = "https://www.amazin.in/"
+        #Load amazon.in site
         cls.driver.get("https://www.amazon.in/")
         cls.driver.maximize_window()
 
 
     def test_amazon(self):
 
-    
-#a.find_element_by_id("searchDropdownBox").click()
-#Select(a.find_element_by_id("searchDropdownBox")).select_by_visible_text("Books") 
+        # In the search box, enter ' data catalog' and search'
         self.driver.find_element_by_xpath("//input[@id='twotabsearchtextbox']").send_keys("data catalog")
         self.driver.find_element_by_xpath("//input[@id='twotabsearchtextbox']").submit()
         self.driver.implicitly_wait(5)                               
@@ -26,13 +25,21 @@ class Amazon(unittest.TestCase):
         
         first_book_title = "The Condensed Chemical Dictionary; A Reference Volume for All Requiring Quick Access to a Large Amount of Essential Data Regarding Chemicals, and ... Used in Manufacturing and Laboratory Work"
 
-        title = self.driver.find_element_by_xpath('//*[@id="result_0"]/div/div/div/div[2]/div[1]/div[1]/a/h2').text
+        #check for title
+        title = self.driver.find_element_by_xpath('//*[@id="result_0"]/div/div/div/div[2]/div[1]/div[1]/a/h2').text.strip()
         self.assertEqual(first_book_title,title)
-        
-        
+    
+        # check for author
         author = self.driver.find_element_by_xpath('//*[@id="result_0"]/div/div/div/div[2]/div[1]/div[2]/span[2]').text
-        paperback = self.driver.find_element_by_xpath('//*[@id="result_0"]/div/div/div/div[2]/div[2]/div[1]/div[2]/a/span').text
-        hardcover = self.driver.find_element_by_xpath('//*[@id="result_0"]/div/div/div/div[2]/div[2]/div[1]/div[7]/a/span').text
+        self.assertEqual(author,"Inc Chemical Catalog Company")
+       
+        # check for paperback price 
+        paperback = self.driver.find_element_by_xpath('//*[@id="result_0"]/div/div/div/div[2]/div[2]/div[1]/div[2]/a/span').text.strip()
+        self.assertEqual(paperback,"1,499")
+        
+        # check for hardcover price
+        hardcover = self.driver.find_element_by_xpath('//*[@id="result_0"]/div/div/div/div[2]/div[2]/div[1]/div[7]/a/span').text.strip()
+        self.assertEqual(hardcover,"3,115.91")
 
         print("Title = ", title)
         print("Author = ", author)
@@ -41,6 +48,7 @@ class Amazon(unittest.TestCase):
         
     @classmethod    
     def tearDown(cls):
+        #Close the browser
         cls.driver.quit()
 
 
